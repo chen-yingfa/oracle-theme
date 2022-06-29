732c39f053acb5c2c5e47767c01c4124b18156d5
Donny Chan <chenyingfa1999@qq.com>
2022-06-29 17:29:05 +0800

    add split

---
+++ data/split.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import json
+import random
+
+
+data_dir = Path('220629')
+src_file = data_dir / 'ancient_220629.json'
+
+data = json.load(open(src_file, encoding='utf8'))
+
+# Shuffle and split data
+random.seed(0)
+random.shuffle(data)
+cnt = len(data)
+splits = [int(cnt * 0.9), int(cnt * 0.95)]
+print(f'Num of examples: {cnt}')
+print(f'Splits: {splits}')
+
+train_data = data[:splits[0]]
+dev_data = data[splits[0]:splits[1]]
+test_data = data[splits[1]:]
+
+print(f'train: {len(train_data)}')
+print(f'dev: {len(dev_data)}')
+print(f'test: {len(test_data)}')
+
+print(f'Dumping to directory: {data_dir}')
+json.dump(
+    train_data,
+    open(data_dir / 'train.json', 'w', encoding='utf8'), 
+    ensure_ascii=False, indent=2)
+json.dump(
+    dev_data,
+    open(data_dir / 'dev.json', 'w', encoding='utf8'), 
+    ensure_ascii=False, indent=2)
+json.dump(
+    test_data,
+    open(data_dir / 'test.json', 'w', encoding='utf8'), 
+    ensure_ascii=False, indent=2)
