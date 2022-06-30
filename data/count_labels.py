0e498cb609f64716e8c691cc14c2f21a9403b701
Donny Chan <chenyingfa1999@qq.com>
2022-07-01 01:56:12 +0800

    Collapse labels into clusters

--- data/count_labels.py
+++ data/count_labels.py
@@ -1,26 +1,36 @@
 from pathlib import Path
 import json
 
-from utils import load_jsonl
+def load_jsonl(filename) -> list:
+    return [json.loads(line) for line in open(filename, 'r', encoding='utf-8')]
 
+def load_label_map() -> dict:
+    clusters = json.load(open('labels/label_clusters.json', encoding='utf-8'))
+    map = {}
+    for name, labels in clusters.items(): 
+        for label in labels:
+            map[label] = name
+    return map
 
 # Load data
-data_dir = Path('data/data')
+data_dir = Path('./data')
 # files = ['train.json', 'dev.json', 'test.json']
 # examples = sum([load_jsonl(data_dir / f) for f in files], [])
 files = ['ancient_0.json', 'ancient_1.json']
 examples = sum([json.load(open(data_dir / f, encoding='utf8')) for f in files], [])
 themes = [x['theme'] for x in examples]
+label_map = load_label_map()
 
 # Get all labels
 labels = {}  # name -> occurrence
 for i, theme in enumerate(themes):
-    theme = [t.replace(' ', '') for t in theme.split('/')][:1]
+    theme = [t.replace(' ', '') for t in theme.split('/')]
     for t in theme:
-        # if t == '行为':
-        #     print(i)
-        #     exit()
         t = t.strip()
+        if t in label_map:
+            t = label_map[t]
+        else:
+            t = ''
         if t not in labels:
             labels[t] = 0
         labels[t] += 1
---
+++ data/labels/map_labels.py
@@ -0,0 +1,50 @@
+import json
+
+THEMES = [line.strip() for line in open('themes.txt', 'r', encoding='utf-8')]
+
+def load_label_map(file):
+    map = {}
+    def add_to_map(key, val):
+        # Map to only one label
+        if label not in THEMES:
+            print(label)
+            print(orig_label)
+            print(line)
+            exit()
+        map[key] = val
+
+    with open(file, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.rstrip()
+            orig_label = line.split('\t')[0]
+            if '-' in line:
+                label = line.split('-')[1]
+            else:
+                label = ''
+            if label not in ['', 'x']:
+                if '；' in label:
+                    labels = label.split('；')
+                    for label in labels:
+                        add_to_map(orig_label, label)
+                else:
+                    add_to_map(orig_label, label)
+    return map
+
+
+
+label_map = load_label_map('label_map_0.txt')
+print(label_map)
+
+clusters = {}
+for raw, name in label_map.items():
+    if name not in clusters:
+        clusters[name] = []
+    clusters[name].append(raw)
+
+# print(json.dumps(clusters, indent=4, ensure_ascii=False))
+print(len(clusters))
+print(clusters.keys())
+clusters_file = 'label_clusters.json'
+json.dump(
+    clusters, open(clusters_file, 'w', encoding='utf8'), 
+    indent=2, ensure_ascii=False)
